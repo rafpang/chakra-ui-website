876932bac5237cf1c05f94a71d0b0171d8e18378
rafpang <rafibintangpagi@gmail.com>
2023-02-13 16:21:39 +0800

    added filler to bottom

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Projects from '@/components/Projects'
 import Skills from '@/components/Skills'
 import About from '@/components/About'
 import Education from '@/components/Education'
+import { Box } from '@chakra-ui/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,7 +22,10 @@ export default function Home() {
    <Education />
    <Skills />
    <Projects />
- 
+  <Box bg={"#212226"}
+     w='100%' p={0}  
+     maxW={"100vw"}
+    minH={"370px"} />
    
    </>
   )
